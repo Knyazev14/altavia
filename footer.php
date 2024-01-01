e619e6b48b6c1fa8e90360534118b72b35b2f3fd
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2024-01-01 18:59:26 +0300

    фул адаптив

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer__body">
+            <div class="footer__top">
+                <a class="footer__logo" href="/">
+                    <?php get_template_part('template/logo/logo') ?>
+                </a>
+                <div class="footer__social">
+                    <?php get_template_part('template/social/telegram') ?>
+                    <?php get_template_part('template/social/youtube') ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </div>
 </body>
 <?php wp_footer(); ?>
--- template/social/youtube.php
+++ template/social/youtube.php
@@ -1,7 +1,7 @@
-<a class="sotial-youtube" href="#">
+<a class="social-youtube" href="#">
 <svg xmlns="http://www.w3.org/2000/svg" width="27" height="21" viewBox="0 0 27 21" fill="none">
-  <rect class="sotial-youtube__rect" width="27" height="21" rx="2" fill="url(#1)"/>
-  <path class="sotial-youtube__path" d="M9.5 6.81953L17.2641 10.814L9.5 14.8085V6.81953Z" stroke="white"/>
+  <rect class="social-youtube__rect" width="27" height="21" rx="2" fill="url(#1)"/>
+  <path class="social-youtube__path" d="M9.5 6.81953L17.2641 10.814L9.5 14.8085V6.81953Z" stroke="white"/>
   <defs>
     <linearGradient id="1" x1="13.5" y1="0" x2="13.5" y2="21" gradientUnits="userSpaceOnUse">
       <stop stop-color="#2C4646"/>
