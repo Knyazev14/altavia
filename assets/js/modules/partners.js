b848e6b4873e5acc91b28696c1e2930ec8d5f818
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2024-01-05 17:09:48 +0300

    remaster partners

--- assets/js/modules/partners.js
+++ assets/js/modules/partners.js
@@ -32,33 +32,4 @@ export function partnersSlider() {
     rewind: true
   });
 
-
-  const allSlides = swiper.slides.concat(swiper.cloneSlides);
-
-  console.log(allSlides)
-  allSlides.forEach((slide, index) => {
-    console.log(slide, index)
-    // Проверяем, есть ли следующий слайд и не является ли текущий слайд последним
-    if (index + 1 < allSlides.length) {
-        if (index % 2 === 0) { // Если текущий слайд четный
-            if ((index + 1) % 2 !== 0) { 
-                // Если следующий слайд нечетный и его значение не равно "0"
-                slide.classList.add('even');
-            } else if ((index + 1) % 2 === 0) { 
-                // Если следующий слайд четный и его значение не равно "0"
-                slide.classList.add('odd');
-            }
-        } else { // Если текущий слайд нечетный
-            if ((index + 1) % 2 === 0) { 
-                // Если следующий слайд четный и его значение не равно "0"
-                slide.classList.add('odd');
-            } else if ((index + 1) % 2 !== 0) { 
-                // Если следующий слайд нечетный и его значение не равно "0"
-                slide.classList.add('even');
-            }
-        }
-    }
-});
-
-
 }
