56aebaf56c73a1d49d5d205901493920ed63b8d9
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2024-01-05 17:01:29 +0300

    добавлено нам доверяюют

--- assets/js/modules/partners.js
+++ assets/js/modules/partners.js
@@ -1,13 +1,64 @@
 export function partnersSlider() {
-    const swiper = new Swiper('.partners__slider', {
-        slidesPerView: 5,
-        spaceBetween: 90,
-        loop: true,
-        freeMode: true,
-        speed: 1500,
-        autoplay: {
-          enabled: true,
-          delay: 0,
-        },
-      });
+  const swiper = new Swiper('.partners__slider', {
+    slidesPerView: 5,
+    spaceBetween: 70,
+     speed: 1000,
+    breakpoints: {
+      420: {
+        spaceBetween: 110
+      },
+      500: {
+        spaceBetween: 120
+      },
+      590: {
+        spaceBetween: 140
+      },
+      768: {
+        spaceBetween: 70
+      },
+      1360: {
+        slidesPerView: 8,
+        spaceBetween: 50
+      },
+    },
+    loop: true,
+    freeMode: true,
+    grabCursor: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true
+    },
+    freeModeMomentum: false,
+    rewind: true
+  });
+
+
+  const allSlides = swiper.slides.concat(swiper.cloneSlides);
+
+  console.log(allSlides)
+  allSlides.forEach((slide, index) => {
+    console.log(slide, index)
+    // Проверяем, есть ли следующий слайд и не является ли текущий слайд последним
+    if (index + 1 < allSlides.length) {
+        if (index % 2 === 0) { // Если текущий слайд четный
+            if ((index + 1) % 2 !== 0) { 
+                // Если следующий слайд нечетный и его значение не равно "0"
+                slide.classList.add('even');
+            } else if ((index + 1) % 2 === 0) { 
+                // Если следующий слайд четный и его значение не равно "0"
+                slide.classList.add('odd');
+            }
+        } else { // Если текущий слайд нечетный
+            if ((index + 1) % 2 === 0) { 
+                // Если следующий слайд четный и его значение не равно "0"
+                slide.classList.add('odd');
+            } else if ((index + 1) % 2 !== 0) { 
+                // Если следующий слайд нечетный и его значение не равно "0"
+                slide.classList.add('even');
+            }
+        }
+    }
+});
+
+
 }
