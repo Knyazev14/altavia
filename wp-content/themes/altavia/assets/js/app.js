23a46367215cb5fdd5b445c71b17b01f2f9eb237
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2024-01-09 19:32:58 +0300

    3 anime

--- wp-content/themes/altavia/assets/js/app.js
+++ wp-content/themes/altavia/assets/js/app.js
@@ -1,14 +1,19 @@
-import * as  headerMenu  from "./modules/menu.js"
-import * as  notice  from "./modules/notice.js"
-import * as  projectOffice  from "./modules/project-office.js"
-import * as  servicesCards  from "./modules/services.js"
-import * as  victoriesFunc  from "./modules/victories.js"
-import * as  partnersSlider  from "./modules/partners.js"
-import * as  pressFunc  from "./modules/press.js"
+import * as  headerMenu from "./modules/menu.js"
+import * as  notice from "./modules/notice.js"
+import * as  projectOffice from "./modules/project-office.js"
+import * as  servicesCards from "./modules/services.js"
+import * as  victoriesFunc from "./modules/victories.js"
+import * as  partnersSlider from "./modules/partners.js"
+import * as  pressFunc from "./modules/press.js"
+import * as  observeAnime from "./modules/observeAnime.js"
 
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
+    //Нам доверяют
+    partnersSlider.partnersSlider()
+    //Нам доверяют
+    observeAnime.observeAnime()
     // Навигация
-    headerMenu.headerMenu() 
+    headerMenu.headerMenu()
     //Рассылка
     notice.notice()
     //Проектный офис
@@ -17,8 +22,6 @@ document.addEventListener('DOMContentLoaded',function(){
     servicesCards.servicesCards()
     //Наши победы
     victoriesFunc.victoriesFunc()
-    //Нам доверяют
-    partnersSlider.partnersSlider()
     //Пресс-центр
     pressFunc.pressFunc()
 })
--- wp-content/themes/altavia/template/pages/main/blocks/hero-animation.php
+++ wp-content/themes/altavia/template/pages/main/blocks/hero-animation.php
@@ -6,3 +6,13 @@
     <img class="hero-main__shard shard-6" src="<?php bloginfo('template_url'); ?>/assets/img/bg/shard-6.svg" alt="Осколок">
     <img class="hero-main__shard shard-7" src="<?php bloginfo('template_url'); ?>/assets/img/bg/shard-7.svg" alt="Осколок">
     <img class="hero-main__shard shard-8" src="<?php bloginfo('template_url'); ?>/assets/img/bg/shard-8.svg" alt="Осколок">
+
+    <div class="preloader-logo">
+        <img class="preloader-logo__img preloader-logo__img-1" src="<?php bloginfo('template_url'); ?>/assets/img/bg/letter-1.svg" alt="Буква">
+        <img class="preloader-logo__img preloader-logo__img-2" src="<?php bloginfo('template_url'); ?>/assets/img/bg/letter-2.svg" alt="Буква">
+        <img class="preloader-logo__img preloader-logo__img-3" src="<?php bloginfo('template_url'); ?>/assets/img/bg/letter-3.svg" alt="Буква">
+        <img class="preloader-logo__img preloader-logo__img-4" src="<?php bloginfo('template_url'); ?>/assets/img/bg/letter-4.svg" alt="Буква">
+        <img class="preloader-logo__img preloader-logo__img-5" src="<?php bloginfo('template_url'); ?>/assets/img/bg/letter-5.svg" alt="Буква">
+        <img class="preloader-logo__img preloader-logo__img-6" src="<?php bloginfo('template_url'); ?>/assets/img/bg/letter-6.svg" alt="Буква">
+        <img class="preloader-logo__img preloader-logo__img-7" src="<?php bloginfo('template_url'); ?>/assets/img/bg/letter-7.svg" alt="Буква">
+    </div>
